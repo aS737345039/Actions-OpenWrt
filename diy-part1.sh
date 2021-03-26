a9ef77fa28e283918fe26706b9cf20e32ae4cf15
枫雨 <737345039@qq.com>
2021-03-26 15:21:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/GJXS1980/ODP.git package/drcomODP
+git clone https://github.com/CHN-beta/xmurp-ua.git package/xmurp-ua
